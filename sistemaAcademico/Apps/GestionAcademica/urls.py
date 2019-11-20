a9a03dd466dbcb3428f0e16e8d2664ea8ac2ab91
Madhorns4 <52832651+Madhorns4@users.noreply.github.com>
2019-11-20 17:28:13 -0600

    restablecer proyecto

--- sistemaAcademico/Apps/GestionAcademica/urls.py
+++ sistemaAcademico/Apps/GestionAcademica/urls.py
@@ -2,36 +2,27 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('login/',login,name='login'),
     path('base/',base, name = 'base'),
     path('inicio/',inicio,name='inicio'),
-    path('login/',login,name='login'),
 
     #Opciones del modulo de Configuaciones:
+    path('usuarios/',usuarios,name='usuarios'),
+    path('roles/',roles,name='roles'),
     path('perfiles/',perfiles,name='perfiles'),
-    path('menus/', lista_menus,name='menu'),
-    path('modulos/',lista_modulos,name='modulo'),
+    path('menu/',menu,name='menu'),
+    path('modulo/',modulo,name='modulo'),
     path('acciones/',acciones,name='acciones'),
-    path('permisos/', lista_permisos, name='permisos'),
-    #path('roles/', lista_rol, name='rol'),
-    path('empresas/', lista_em, name='empresas'),
-    path('usuarios/', lista_us, name='usuarios'),
-
-    #formularios de registro
-    path('add_empresa/', addempresa, name='add_empresa'),
-    path('add_rol/', addrol, name='add_rol'),
-    path('add_usuario/', addusuario, name='add_usuario'),
-
-    #formularios de edicion
-    path('edit_usuario', editar_usuario, name='edit_usuario'),
-
     #----------------------------------------
 
     #Opciones para el modulo de Admision
-    path('mantenimiento personas/',mantenimientoPersonas,name='mantenimiento_personas'),
+    path('mantenimiento personas/',mantenimientoPersonas,name='mantenimientoPersonas'),
     path('movimientos/',movimientos, name='movimientos'),
     path('consultas/',consultas, name='consultas'),
     path('procesos/',procesos, name='procesos'),
     path('reportes/',reportes, name='reportes'),
     #--------------------------------------
+
+    path('salir/',salir,name='logout')
 
 ]
