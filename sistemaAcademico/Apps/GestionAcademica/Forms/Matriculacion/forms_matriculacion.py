04b9695d2a24ea90cf53d75aabfcfd619ce05fa0
josuemmg <55563268+josuemmg@users.noreply.github.com>
2020-03-27 15:36:22 -0500

    ya fue ---solo creo una class

--- sistemaAcademico/Apps/GestionAcademica/Forms/Matriculacion/forms_matriculacion.py
+++ sistemaAcademico/Apps/GestionAcademica/Forms/Matriculacion/forms_matriculacion.py
@@ -18,3 +18,18 @@ class Aniolectivo (forms.ModelForm):
                    'fecha_incio_ciclo': forms.DateTimeInput(attrs={"class": "form-control text-dark", "type": "date"}),
                    'fecha_fin_ciclo': forms.DateTimeInput(attrs={"class": "form-control text-dark", "type": "date"})}
 
+class UpAniolectivo (forms.ModelForm):
+    class Meta:
+        model = MantAnioLectivo
+        fields = ['anio',
+                  'ciclo',
+                  'fecha_incio_ciclo',
+                  'fecha_fin_ciclo']
+        labels = {'anio': 'Año',
+                  'ciclo': 'Ciclo',
+                  'fecha_incio_ciclo': 'Inicio del ciclo',
+                  'fecha_fin_ciclo': 'Fin del ciclo'}
+        widgets = {'anio': forms.NumberInput(attrs={"class": "form-control"}),
+                   'ciclo': forms.NumberInput(attrs={"class": "form-control"}),
+                   'fecha_incio_ciclo': forms.DateTimeInput(attrs={"class": "form-control text-dark"}),
+                   'fecha_fin_ciclo': forms.DateTimeInput(attrs={"class": "form-control text-dark"})}
