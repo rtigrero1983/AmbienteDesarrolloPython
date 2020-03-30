8ad726bc47d4759480890cda170739a59932494b
SjBurgos <56896745+SjBurgos@users.noreply.github.com>
2020-03-30 13:44:20 -0600

    sjbm

--- sistemaAcademico/Apps/GestionAcademica/Controladores/Matriculacion/asignacion_curso.py
+++ sistemaAcademico/Apps/GestionAcademica/Controladores/Matriculacion/asignacion_curso.py
@@ -1,7 +1,46 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from sistemaAcademico.Apps.GestionAcademica.Forms.Matriculacion.forms_matriculacion import *
+from sistemaAcademico.Apps.GestionAcademica.Forms.Matriculacion.forms_mov_anio_curso import Mov_Aniolectivo_curso_forms
 from django.shortcuts import render, redirect
+from sistemaAcademico.Apps.GestionAcademica.Diccionario.Estructuras_tablas_mov import Mov_Aniolectivo_curso
+from sistemaAcademico.Apps.GestionAcademica.Diccionario.Estructuras_tablas_genr import GenrGeneral
+
+
+
+class Create_Mov_Aniolectivo_curso(CreateView):
+    model = Mov_Aniolectivo_curso
+    form_class = Mov_Aniolectivo_curso_forms
+    template_name = 'sistemaAcademico/Matriculacion/Asignacion_curso/crear_asig_curso.html'
+    success_url = reverse_lazy('Academico:asignacion_curso')
+class Update_Mov_Aniolectivo_curso(UpdateView):
+    model = Mov_Aniolectivo_curso
+    form_class = Mov_Aniolectivo_curso_forms
+    context_object_name = 'anio'
+    template_name = 'sistemaAcademico/Matriculacion/Asignacion_curso/edit_asig_curso.html'
+    success_url = reverse_lazy('Academico:asignacion_curso')
+class ListaAnioElectivoCurso(ListView):
+    model = Mov_Aniolectivo_curso
+    context_object_name = 'anio'
+    queryset = Mov_Aniolectivo_curso.objects.filter(id_estado_gnral=97)
+    template_name = 'sistemaAcademico/Matriculacion/Asignacion_curso/listar_asig_curso.html'
+def eliminar_Asignacion_Curso(request, id):
+    anio = Mov_Aniolectivo_curso.objects.get(id_mov_anioelectivo_curso=id)
+    inactivo = GenrGeneral.objects.get(idgenr_general=98)
+    if request.method == 'POST':
+        anio.id_estado_gnral = inactivo
+        anio.save()
+        return redirect('Academico:asignacion_curso')
+    return render(request, 'sistemaAcademico/Matriculacion/Asignacion_curso/eliminar_asig_curso.html', {'anio': anio})
+class Eliminar_Asignacion_Curso(DeleteView):
+    model = Mov_Aniolectivo_curso
+    context_object_name = 'anio'
+    queryset = Mov_Aniolectivo_curso.objects.filter(id_estado_gnral=97)
+    template_name = 'sistemaAcademico/Matriculacion/Asignacion_curso/listar_asig_curso.html'
+
+
+
+
+
 def crear_asig_curso(request):
     return render(request,'sistemaAcademico/Matriculacion/Asignacion_curso/crear_asig_curso.html')
 def edit_asig_curso(request):
