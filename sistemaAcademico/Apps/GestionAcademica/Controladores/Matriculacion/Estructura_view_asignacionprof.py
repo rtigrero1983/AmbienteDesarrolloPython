a0bb6abe6e4ee4bef377766aaa18efb19b4f3350
roddytc <ricar.tig@gmail.com>
2020-11-06 15:28:04 -0500

    Cambios en modulo Matriculacion (Asignaciones/Materia/Profesor)

--- sistemaAcademico/Apps/GestionAcademica/Controladores/Matriculacion/Estructura_view_asignacionprof.py
+++ sistemaAcademico/Apps/GestionAcademica/Controladores/Matriculacion/Estructura_view_asignacionprof.py
@@ -15,43 +15,35 @@ from django.views.decorators.cache import cache_page
 class List_docente(ListView):
     model = Mov_Materia_profesor
     template_name = 'sistemaAcademico/reportes/AsignacionProfesores.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(List_docente, self).get_context_data(**kwargs)
-        lista = []
-            
-        queryset = self.model.objects.filter(
-        id_empleado__id_persona__estado=97).prefetch_related('id_detalle_materia_curso')
-        count=1
-        for materia in queryset:
-            print(materia)
-            roles = materia.id_detalle_materia_curso.all()
-            for role in roles:
-                lista.append((count,materia.id_empleado,role.id_genr_materias.nombre,role.id_mov_anio_lectivo_curso,materia))
-            count+=1
-        context['lista_profesor']   =lista   
-        return context
-
+    context_object_name = 'lista_profesor'
+    queryset = Mov_Materia_profesor.objects.filter(id_empleado__id_persona__estado=97)
 
 
 class List_docente_asignado(ListView):
     model = Mov_Materia_profesor
     template_name = 'sistemaAcademico/reportes/ProfesoresAsignados.html'
-    context_object_name = 'lista_profesor_asignados'
-    queryset = Mov_Materia_profesor.objects.filter(id_empleado__id_persona__estado = 97)
-    def get(self, request):
-        context = {'lista_profesor_asignados': None}
-        profesores = []
-        for q in self.queryset:
-            if q.id_detalle_materia_curso.exists():
-                profesores.append(q)
-                print("kjkjkjk", profesores)
-        context['lista_profesor_asignados'] = profesores
-        return render(request, self.template_name, context)
-       
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(List_docente_asignado, self).get_context_data(**kwargs)
+        lista = []
+
+        queryset = self.model.objects.filter(
+            id_empleado__id_persona__estado=97).prefetch_related('id_detalle_materia_curso')
+        count = 1
+        for materia in queryset:
+            print(materia)
+            roles = materia.id_detalle_materia_curso.all()
+            for role in roles:
+                lista.append(
+                    (count, materia.id_empleado, role.id_genr_materias.nombre, role.id_mov_anio_lectivo_curso, materia))
+            count += 1
+        context['lista_profesor'] = lista
+        return context
+
+
 class List_docente_sin_asignar(ListView):
     model = Mov_Materia_profesor
-    template_name = 'sistemaAcademico/reportes/AsignacionProfesores.html'
+    template_name = 'sistemaAcademico/reportes/ProfesorSinAsignar.html'
     context_object_name = 'lista_profesor_sin_asignar'
     queryset = Mov_Materia_profesor.objects.filter(id_empleado__id_persona__estado = 97)
     def get(self, request):
